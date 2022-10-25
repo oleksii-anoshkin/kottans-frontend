7c16c77c3339fd13979a4f4fd5af11be85133196
Oleksii Anoshkin <anoshkinaleksey@gmail.com>
2022-10-25 23:25:39 +0300

    Практика.
    Створив "бургер меню" та активні стрілки.

--- task_responsive_web_design/practise other/script.js
+++ task_responsive_web_design/practise other/script.js
@@ -1,7 +1,16 @@
 "use strict";
 
 $(document).ready(function () {
+  $('.burger__box').click(function (event) {
+    $('.burger__box, .nav__links, .burger').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
   $('.link__item-header').click(function (event) {
-    $(this).toggleClass('active').next().slideToggle(300)
-  })
+    // if ($('.block').hasClass('one')){
+    //   $('.link__item-header').not($(this)).removeClass('active');
+    //   $('link__item-text').not($(this).next()).slideUp(300);
+    // }
+    $(this).toggleClass('active').next().slideToggle(300);
+  });
 });
